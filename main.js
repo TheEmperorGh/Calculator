6a54541995c7bdc8862c6f3743014eef3d8817c0
Md Mazharul Islam Samrat <105455837+TheEmperorGh@users.noreply.github.com>
2023-07-23 22:48:29 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
-function insertChar(char) {
-  document.getElementById('result').value += char;
+let currentExpression = '';
+
+function appendNumber(number) {
+  currentExpression += number;
+  updateDisplay();
 }
 
-function clearResult() {
-  document.getElementById('result').value = '';
+function appendOperator(operator) {
+  currentExpression += operator;
+  updateDisplay();
 }
 
-function deleteChar() {
-  var result = document.getElementById('result').value;
-  document.getElementById('result').value = result.slice(0, -1);
+function appendFunction(func) {
+  currentExpression += func + '(';
+  updateDisplay();
+}
+
+function appendDecimal() {
+  if (currentExpression !== '' && !currentExpression.includes('.')) {
+    currentExpression += '.';
+    updateDisplay();
+  }
+}
+
+function clearDisplay() {
+  currentExpression = '';
+  updateDisplay();
+}
+
+function deleteLastChar() {
+  currentExpression = currentExpression.slice(0, -1);
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = currentExpression;
 }
 
 function calculate() {
-  var result = document.getElementById('result').value;
   try {
-    var calculatedResult = eval(result);
-    document.getElementById('result').value = calculatedResult;
+    currentExpression = eval(currentExpression);
+    updateDisplay();
   } catch (error) {
-    document.getElementById('result').value = 'Error';
+    currentExpression = 'Error';
+    updateDisplay();
   }
 }
+
